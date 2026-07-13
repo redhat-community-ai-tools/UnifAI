2e71f9c24153892527606363527b3457f1458238
nrashti <nrashti@redhat.com>
2026-07-13 07:30:14 -0400

    Maya | CR adjustment

--- ui/client/src/utils/sessionHelpers.ts
+++ ui/client/src/utils/sessionHelpers.ts
@@ -45,7 +45,7 @@ export const transformSessionData = (
   const blueprintId = sessionData.blueprint_id;
   const blueprintExists = sessionData.blueprint_exists;
   const fromSharedLink = sessionData.metadata?.source === 'public_link';
-  const hitlEnabled = sessionData.metadata?.hitl_enabled === true || sessionData.metadata?.hitlEnabled === true;
+  const hitlEnabled = sessionData.metadata?.hitl_enabled === true;
   const activityTimestamp = sessionData.last_active_at || sessionData.started_at;
   const timestamp = new Date(activityTimestamp);
   const lastActive = formatRelativeTimestamp(activityTimestamp);
